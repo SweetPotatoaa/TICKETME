27ecc8a29b0bcc893448a0c67d6fd3cb8e7d03cb
Aurélia <aurelia.ngau@gmail.com>
2020-12-21 21:11:25 +0100

    Dashboard and small changes

--- app/src/main/java/com/example/loginsqlite/DatabaseHelper.java
+++ app/src/main/java/com/example/loginsqlite/DatabaseHelper.java
@@ -1,4 +1,71 @@
 package com.example.loginsqlite;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper (Context context){ super(context, "Login.db", null, 1);}
+
+    @Override
+    public void onCreate(SQLiteDatabase MyDB) {
+        //this will create a table with 2 colums username and password
+        MyDB.execSQL("create Table users(username TEXT primary key, password TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDB, int oldVersion, int newVersion) {
+        MyDB.execSQL("drop Table if exists users");
+
+    }
+
+    //method to insert the data
+    public Boolean insertData(String username, String password){
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long result = MyDB.insert("users",null, contentValues);
+        if (result==-1) return false;
+        else
+            return true;
+    }
+
+    public Boolean updatepassword(String username, String password){
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put("password", password);
+        long result = MyDB.update("users", contentValues, "username = ?", new String[] {username});
+        if (result==-1) return false;
+        else
+            return true;
+    }
+
+    //check if the username already exists or not in the table
+    public Boolean checkusername(String username){
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        Cursor cursor = MyDB.rawQuery("Select * from users where username = ?", new String[] {username});
+        //if the user exists
+        if (cursor.getCount()>0)
+            return true;
+        else
+            return false;
+    }
+
+    //check the password
+    public Boolean checkusernamepassword(String username, String password){
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        Cursor cursor = MyDB.rawQuery("Select * from users where username = ? and password = ?", new String[]{username, password});
+        if (cursor.getCount()>0)
+            return true;
+        else
+            return false;
+    }
+
 }
--- app/src/main/java/com/example/loginsqlite/HomeMenu.java
+++ app/src/main/java/com/example/loginsqlite/HomeMenu.java
@@ -1,14 +1,42 @@
 package com.example.loginsqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeMenu extends AppCompatActivity {
+
+    CardView cardTicket;
+    CardView cardStat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_menu);
+
+        cardTicket = findViewById(R.id.cardticket);
+        cardStat = findViewById(R.id.cardstat);
+
+
+        cardTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //permet d'aller à la page MyTickets
+                Intent intent = new Intent(getApplicationContext(), MyticketsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardStat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //permet d'aller à la page Statistiques
+                Intent intent = new Intent(getApplicationContext(), StatActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
