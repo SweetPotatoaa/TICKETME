988e5cc0692b65aaee34b9447e7cc2661f01ef9f
Aurélia <aurelia.ngau@gmail.com>
2021-01-01 18:56:06 +0100

    Statistics graphs

--- app/src/main/java/com/example/loginsqlite/StatActivity.java
+++ app/src/main/java/com/example/loginsqlite/StatActivity.java
@@ -2,7 +2,9 @@ package com.example.loginsqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StatActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class StatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat);
+
+        findViewById(R.id.buttonPieChart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DepensesPerShop.class));
+            }
+        });
+
+        findViewById(R.id.buttonBarChart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DepensesByPeriod.class));
+            }
+        });
     }
 }
